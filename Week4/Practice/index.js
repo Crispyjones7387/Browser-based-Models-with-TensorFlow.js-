f25acddc3f498ea2e05ab082bb03538e96a368ee
Crispyjones7387 <calebshibu@gmail.com>
2020-05-09 19:13:07 +0530

    initial commit

---
+++ Week4/Practice/index.js
@@ -0,0 +1,36 @@
+let mobilenet;
+let model;
+const webcam = new Webcam(document.getElementById('wc'));
+const dataset = new RPSDataset();
+var rockSamples=0, paperSamples=0, scissorsSamples=0;
+let isPredicting = false;
+
+async function loadMobilenet() {
+  const mobilenet = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+  const layer = mobilenet.getLayer('conv_pw_13_relu');
+  return tf.model({inputs: mobilenet.inputs, outputs: layer.output});
+}
+
+async function train() {
+  dataset.ys = null;
+  dataset.encodeLabels(3);
+  model = tf.sequential({
+    layers: [
+      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
+      tf.layers.dense({ units: 100, activation: 'relu'}),
+      tf.layers.dense({ units: 3, activation: 'softmax'})
+    ]
+  });
+  const optimizer = tf.train.adam(0.0001);
+  model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
+  let loss = 0;
+  model.fit(dataset.xs, dataset.ys, {
+    epochs: 10,
+    callbacks: {
+      onBatchEnd: async (batch, logs) => {
+        loss = logs.loss.toFixed(5);
+        console.log('LOSS: ' + loss);
+        }
+      }
+   });
+}
